15c54843490bfea1a3d0f6af1bb8d9abf478295b
Felix Ruiz de Arcaute <felix@Felixs-MacBook-Air.local>
2017-03-23 15:57:22 +0100

    extending the model() hook to deal with fastboot calls

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    fastboot: Ember.inject.service(),
     model() {
+        if (this.get('fastboot.isFastBoot')) {
+            return this.store.findAll('blog-post', {include: "comments"});
+        }
         return this.store.findAll('blog-post');
     }
 });
